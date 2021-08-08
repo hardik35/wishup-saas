39c97023416eb2dee04fc6d3d61c485c0969b601
hardik35 <hardikarora355@gmail.com>
2021-08-08 21:48:23 +0530

    added script to add plans data

---
+++ scripts/plans.sql
@@ -0,0 +1,7 @@
+Insert Into "Plans" (id, name, "daysValidity", "costUSD", "createdAt", "updatedAt") 
+VALUES (1, 'FREE', 3650, 0.0, current_timestamp, current_timestamp), 
+(2, 'TRIAL', 7, 0, current_timestamp, current_timestamp),
+(3, 'LITE_1M', 30, 100, current_timestamp, current_timestamp),
+(4, 'PRO_1M', 30, 200, current_timestamp, current_timestamp),
+(5, 'LITE_6M', 180, 500, current_timestamp, current_timestamp),
+(6, 'PRO_6M', 180, 900, current_timestamp, current_timestamp)
